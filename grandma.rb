bf0f3aadea856e49a549a90f15447142bc6a07db
Bryce Ancheta <bryceancheta@gmail.com>
2020-08-18 15:25:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,3 @@
 phrase = "Hi grandma, how are you!"
 
-def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
-  end
-end
+def speak_to_grandma("i love you pumpkin")
