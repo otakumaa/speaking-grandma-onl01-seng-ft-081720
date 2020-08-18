7fbaac47b7af0ffd019822ef08b2b7919e6ead0f
Bryce Ancheta <bryceancheta@gmail.com>
2020-08-18 15:09:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!
-phrase = "H"
+phrase = "Hi grandma!"
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
