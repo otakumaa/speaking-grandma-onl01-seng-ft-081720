60f9ee54e598ecfde79ca1af8c25d9cd24e34c45
Bryce Ancheta <bryceancheta@gmail.com>
2020-08-18 15:43:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,6 @@ def speak_to_grandma(phrase)
   elsif phrase == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == 'WHAT DID YOU EAT TODAY?'
-    
+    'NO, NOT SINCE 1938!'
   end
 end
